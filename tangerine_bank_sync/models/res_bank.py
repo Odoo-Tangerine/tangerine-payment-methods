1f3a9e9a5feed753a8df64d242979e043bd34d2a
Long Duong Nhat <128609047+Odoo-Tangerine@users.noreply.github.com>
2024-08-29 19:49:44 +0700

    Update res_bank.py

--- tangerine_bank_sync/models/res_bank.py
+++ tangerine_bank_sync/models/res_bank.py
@@ -1,4 +1,3 @@
-import os
 import requests
 from odoo import fields, models, api
 from odoo.exceptions import UserError
@@ -37,12 +36,6 @@ class ResBank(models.Model):
             if res.status_code == 200:
                 result = res.json()
                 for rec in result.get('data', []):
-                    file_name = f"{rec.get('code')}-{rec.get('bin')}.png"
-                    file_path = os.path.join(os.path.split(os.path.dirname(__file__))[0], 'static', 'logo', file_name)
-                    if not os.path.exists(file_path):
-                        res_logo = requests.get(url=rec.get('logo'))
-                        with open(file_path, 'wb') as file:
-                            file.write(res_logo.content)
                     bank_id = self.search([('code', '=', rec.get('code'))])
                     if bank_id:
                         bank_id.write(self._payload_bank(rec))
