5f0baa04059431b31e147d992d4b62fa258dbba2
Long Duong Nhat <128609047+Odoo-Tangerine@users.noreply.github.com>
2024-09-06 20:08:10 +0700

    Commit

--- tangerine_sepay_integration/models/account_move.py
+++ tangerine_sepay_integration/models/account_move.py
@@ -7,5 +7,5 @@ class AccountMove(models.Model):
     def generate_qr_code(self):
         self.ensure_one()
         if self.company_id.country_code == 'VN' and self.qr_code_method == 'sepay_qr':
-            return f'https://qr.sepay.vn/img?acc={self.partner_bank_id.acc_number}&bank={self.partner_bank_id.bank_id.code}&amount={self.amount_residual}&des={self.invoice_origin}'
+            return f'https://qr.sepay.vn/img?acc={self.partner_bank_id.acc_number}&bank={self.partner_bank_id.bank_id.code}&amount={self.amount_residual}&des={self.invoice_origin}&template=compact'
         return super(AccountMove, self).generate_qr_code()
