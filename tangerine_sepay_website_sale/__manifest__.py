ad9c6db79768ad1d6f69dd3547b5c61e42db9266
Long Duong Nhat <128609047+Odoo-Tangerine@users.noreply.github.com>
2024-09-20 17:54:28 +0700

    Commit

--- tangerine_sepay_website_sale/__manifest__.py
+++ tangerine_sepay_website_sale/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'SePay On Website',
     'version': '17.0.1.0',
-    'category': 'Accounting/Payment Providers',
+    'category': 'Hidden',
     'summary': """The SePay On Website Module support QR code initialization on eCommerce. The module is an alternative solution to reduce dependencies when you do not need to use the eCommerce module (website_sale) in SePay Integration.""",
     'depends': ['tangerine_sepay_integration', 'website_sale'],
     'author': 'Long Duong Nhat',
